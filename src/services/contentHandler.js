a0df60d10ad1c7e86ea87f4482361272b7404134
Luis Fernando Lemos <flemosdemoura@gmail.com>
2025-01-06 00:56:38 -0300

    getting tag name

--- src/services/contentHandler.js
+++ src/services/contentHandler.js
@@ -1,9 +1,26 @@
 
 const searchLinks = (htmlContent) => {
 
-    var nospacesContent = htmlContent.replace(/ /g,'');
-    var contentArr = nospacesContent.split(/[\r\n\s]+/);
+    const nospacesContent = htmlContent.replace(/ /g,'');
+    const contentArr = nospacesContent.split(/[\r\n\s]+/);
     console.log(contentArr[0]);
+
+}
+
+const parseJson = (content) =>{
+    const tagNameRegExp = '/<\/?([a-zA-Z][\w-]*)/g';
+    const selfClosingRegex = /<([a-zA-Z][\w-]*)\s*[^>]*\/>/;
+
+    const response = {};
+    let currentItem;
+    let isSelfClosingTag = false;
+
+    for(let i = 0; i < content.length; i++){
+        const match = content[i].test(tagNameRegExp)
+        if(match){
+            console.log(march[1]);
+        }
+    }
 
 }
 
