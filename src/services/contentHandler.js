9ecaa9ccaa947bfd7c7ac820c0938fed4ba29bf3
Luis Fernando Lemos <flemosdemoura@gmail.com>
2025-01-08 23:53:30 -0300

    iterate over dom tree and start to parse to json

--- src/services/contentHandler.js
+++ src/services/contentHandler.js
@@ -22,13 +22,18 @@ const parseJson = (content) =>{
         
         const match = tagNameRegExp.exec(content[i]);
         console.log(match);
+        if(tagNameRegExp.test(content[i])){
 
-        if(match){
+            const match = tagNameRegExp.exec(content[i]);
+            console.log(match);
+
             response[match[1] != '!' ? match[1] : match[2]] = {};
             console.log(response);
+        }else{
+            console.log("Element doesn't match with a valid tag")
         }
 
-        // break;
+        break;
     }
 
 }
